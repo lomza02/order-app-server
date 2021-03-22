be6f0ea4398a5c20fafa06e1009191489f7438ad
ml <mlomzynski94@gmail.com>
2021-03-22 14:22:41 +0100

    setup basic config and add static files

---
+++ src/app.ts
@@ -0,0 +1,16 @@
+import express, { Request, Response, NextFunction } from 'express';
+import { json } from 'body-parser';
+import cors from 'cors';
+
+const app = express();
+app.use(cors());
+app.use(json());
+app.use(express.static('public'));
+
+app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
+  res.status(500).json({ message: err.message });
+});
+
+app.listen(3030, () => {
+  console.log('Server is running on port 3030');
+});
